1f47ee8c21d2c7c9b93ee222fe927e98422b9191
Ellis <ellispinsky5@gmail.com>
2023-03-21 21:08:40 -0400

    updated textToChunks function & generateAnswer

--- pdfGPT.js
+++ pdfGPT.js
@@ -1,6 +1,10 @@
 const pdfParse = require('pdf-parse');
+const fs = require('fs');
 const tf = require('@tensorflow/tfjs-node');
 const use = require('@tensorflow-models/universal-sentence-encoder');
+const OpenAI = require('openai'); // Include OpenAI package
+
+OpenAI.apiKey = 'your_openai_api_key'; // Set your OpenAI API key
 
 let pdfText = '';
 let chunks = [];
@@ -19,11 +23,41 @@ const pdfToText = async (path) => {
 };
 
 const textToChunks = (text) => {
-  // Add logic for splitting text into chunks
+  const maxLength = 2048; // Maximum length for a GPT-3 text input
+  const regex = new RegExp(`.{1,${maxLength}}(\\s|$)`, 'g');
+  return text.match(regex);
 };
 
 const generateAnswer = async (question) => {
-  // Add logic for generating an answer using GPT-3
+  const inputs = chunks.map((chunk) => ({
+    'role': 'document',
+    'content': chunk,
+  }));
+
+  inputs.push({
+    'role': 'system',
+    'content': 'Please answer the following question based on the information provided in the document:',
+  });
+
+  inputs.push({
+    'role': 'user',
+    'content': question,
+  });
+
+  const prompt = {
+    'messages': inputs,
+  };
+
+  const response = await OpenAI.Completion.create({
+    engine: 'text-davinci-002',
+    prompt: JSON.stringify(prompt),
+    max_tokens: 100,
+    n: 1,
+    stop: null,
+    temperature: 0.5,
+  });
+
+  return response.choices[0].text.trim();
 };
 
 module.exports = { pdfToText, textToChunks, generateAnswer };
